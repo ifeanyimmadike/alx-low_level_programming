f0d8bb6a6c3a15f24b53f1bedad41042c2e0b686
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-16 10:23:47 -0700

    5p

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0 ; d <= 9 ; d++)
+	{
+		printf("%d\n",d);
+	}
+	return (0);
+}
