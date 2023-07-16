b4831c8f627704c3d5616a7471acf8530da185dc
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-16 08:30:16 -0700

    3rd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char D;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (D = 'A' ; D <= 'Z' ; D++)
+	{
+		putchar (D);
+	}
+	putchar('\n');
+	return (0);
+}
+
