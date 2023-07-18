000d54830aafc64506f74bf29c71154611520a8d
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-17 18:40:04 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0 ; c < 99 ; c++)
+	{
+		for (d = c + 1 ; d < 100 ; d++)
+		{
+			putchar((c / 10) + '0');
+			putchar((c % 10) + '0');
+			putchar(' ');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+
+			if (c == 98 && d == 99)
+				continue;
+				putchar(',');
+				putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
