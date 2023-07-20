2fccd4b35e35b73bc32468447077fa70a1173ead
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-19 21:31:33 -0700

    o-p

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	printf("%s\n",c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+lets do hard c programming 
