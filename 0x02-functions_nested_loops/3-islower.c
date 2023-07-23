48e43c9ecd6c3801422229b5a433a71f0c4042bd
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-23 08:40:59 -0700

    sec

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_islower - checks a lower case character
+ *@c :parameter to be checked
+ *Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+	return (0);
+}
