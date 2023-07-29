a5e2f415bd558e911f62e4268d028575c192f2bb
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 16:43:03 -0700

    str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i ;
+	
+	for (i = 1 ; s < '\0' ; i++)
+	{
+		s[i];
+	}
+	return (s);
+}
