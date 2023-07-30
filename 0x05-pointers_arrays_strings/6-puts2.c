8f6953ce5b9bf0303e62d6d9ae38ac20d17cc50e
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 18:42:44 -0700

    6put

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2-output single character
+ * @str : string
+ * Return :0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; i < 0 ; i++)
+	{
+		str[i] = out;
+		_putchar(out);
+	}
+	_putchar('\n');
+}
