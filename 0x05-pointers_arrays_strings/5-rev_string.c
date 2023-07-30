77df2676e1ab9f6d5d32302b76ac72e0c590ea99
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 18:19:44 -0700

    stringReversal

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input 
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int n = 0;
+	int i;
+
+	while (s[n] != '\0')
+	n++;
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		rev = s[i];
+		s[i] = s[n];
+		s[n] = rev;
+	}
+}
+
+
