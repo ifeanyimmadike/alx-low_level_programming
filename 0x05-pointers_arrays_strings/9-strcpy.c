770e4c348c4ad0c6a46b7a3e0aadee1c7a509008
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 19:46:42 -0700

    strcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy- copy source to destination(dest)
+ *@dest: destination of copied string
+ *@src: string to be copied
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char m;
+
+	for (i = 0 ; src <= '\0'; i++)
+	{
+		src[i] = m;
+	}
+	for (i = 0 ; dest <= '\0'; i++)
+	{
+		src[i] = dest[i];
+		dest[i] = m;
+	}
+	return (dest);
