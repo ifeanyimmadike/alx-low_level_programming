b1ee7459be9c9f95e96882638008c6416e7aefea
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 17:35:10 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int o;
+
+	for (o = 0; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
