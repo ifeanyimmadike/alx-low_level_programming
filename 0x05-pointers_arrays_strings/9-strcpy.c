74e2cc63bfcd0fdb2437669967620eda2483be4c
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 19:53:40 -0700

    strcopy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,13 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	char m;
 
-	for (i = 0 ; src <= '\0'; i++)
+	for (i = 0 ; src != '\0'; i++)
 	{
 		src[i] = m;
 	}
-	for (i = 0 ; dest <= '\0'; i++)
+	for (i = 0 ; dest != '\0'; i++)
 	{
+		src[i] = m;
 		dest[i] = src[i];
 		dest[i] = m;
 	}
