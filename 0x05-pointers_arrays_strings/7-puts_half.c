089110c748d50ac07070be31a262a0800a31e358
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-29 19:00:23 -0700

    7puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,16 @@
  */
 void puts_half(char *str)
 {
-	int a, n, longi;
+	int a, n, i;
 
 	longi = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
-		longi++;
+		i++;
 
-	n = (longi / 2);
+	n = (i / 2);
+	if ((i % 2) == 1)
+		n = ((i + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
