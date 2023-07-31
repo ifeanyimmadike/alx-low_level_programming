36254f87a1f79e0f1d88b9fdc65d629c6170c635
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-30 21:46:46 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - function to print trigle
+ * @size: size of triangle
+ * Return:0
+ */
+
+void print_triangle(int size)
+{
+	int h, i;
+
+	if (size > 0)
+	{
+		for (hash = 1; h <= size; h++)
+		{
+			for (i = size - h; i > 0; i--)
+				_putchar(' ');
+
+			for (i = 0; i < h; i++)
+				_putchar('#');
+
+			if (h == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
