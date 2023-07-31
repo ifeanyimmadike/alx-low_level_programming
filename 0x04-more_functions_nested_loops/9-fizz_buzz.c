d550f4f4ba74b65bbcda20ec0e09b129e3cd76f8
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-07-30 21:28:29 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,21 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz");
+
 		else if ((n % 3) == 0)
 			printf("Fizz");
+
 		else if ((n % 5) == 0)
 			printf("Buzz");
+
 		else
 			printf("%d", n);
+
 		if (n == 100)
 			continue;
 		printf(" ");
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
 
