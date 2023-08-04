ce94533412f01402fe7059c185d4669bbd52cb5b
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-03 21:00:02 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+/**
+ * _puts_recursion - print string in a new line;
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+
+
