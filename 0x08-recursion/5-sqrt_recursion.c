a22910cfed8bf2f0cb2a016c65c9abdf2647b6d6
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-06 09:12:55 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,12 +6,12 @@
  *
  * Return: 0
  */
-int real_sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (real_sqrt_recursion(n, i + 1));
+	return (_sqrt_recursion(n, i + 1));
 }
 
