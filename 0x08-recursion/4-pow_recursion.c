b43114727bbb8873dd983bff339fa04962f89f19
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-06 07:46:08 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion- function with x^y
+ * @x:input
+ * @y:input
+ * Return:0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1)
+		return (x * *y, _pow_recursion(x, y));
+}
