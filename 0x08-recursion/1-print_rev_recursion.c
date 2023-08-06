0b3800f16053eade6956bf9a647b09e3ce1232b9
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-06 07:15:39 -0700

    print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
-void _puts_recursion(char *s)
+/**
+ * _print_rev_recursion- prints the reverse of a string
+ * @s:character input
+ */
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 		putchar(*s);
 	}
-	return(0);
+	return (0);
 }
