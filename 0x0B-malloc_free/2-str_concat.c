244565910da004307a8868425e26414b4687d335
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-11 15:56:52 -0700

    function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat-function to concatinate two string
+ * @s1:string
+ * @s2:string2
+ *Return:0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, jk;
+	int conct = 0;
+	int len1, len2 = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (" ");
+	while (s1[len1] !=  '\0')
+		len1++;
+	while (s2[len2] !=  '\0')
+		len2++;
+	conct = malloc(sizeof(char) * (i + jk + 1);
+	if (conct == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		conct[i] = s1[i];
+	}
+	for (jk = 0; jk < len2; jk++)
+	{
+		conct[len1 + jk] = s2[jk];
+	}
+	conct = '\0';
+	return (conct);
+}
