c6cc3ab6111daa1d979d7115c24796ac2088be6f
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-11 16:14:04 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,33 +9,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i, jk;
-    int len1 = 0, len2 = 0;
-    char *conct;
+	int i, jk;
+	int len1 = 0, len2 = 0;
+	char *conct;
 
-    if (s1 == NULL || s2 == NULL)
-        return NULL;
+	if (s1 == NULL || s2 == NULL)
+	return (NULL);
+	while (s1[len1] != '\0')
+	len1++;
+	while (s2[len2] != '\0')
+	len2++;
+	conct = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (conct == NULL)
+	return (NULL);
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
-
-    conct = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-    if (conct == NULL)
-        return NULL;
-
-    for (i = 0; i < len1; i++)
-    {
-        conct[i] = s1[i];
-    }
-    for (jk = 0; jk < len2; jk++)
-    {
-        conct[len1 + jk] = s2[jk];
-    }
-
-    conct[len1 + len2] = '\0';
-
-    return conct;
+	for (i = 0; i < len1; i++)
+	{
+		conct[i] = s1[i];
+	}
+	for (jk = 0; jk < len2; jk++)
+	{
+		conct[len1 + jk] = s2[jk];
+	}
+	conct[len1 + len2] = '\0';
+	return (conct);
 }
 
