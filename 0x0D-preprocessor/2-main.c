f9f03ce4e9f54012d4307a31930c69c89efc170a
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-15 19:46:49 -0700

    a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#ifndef ma_H
+#define ma_H
+#include <stdio.h>
+#define 2-main.c 
+
+/**
+ * mainf-function name
+ *
+ * Return:0
+ */
+int mainf(void)
+{
+	printf("%d", 2-main.c);
+	return (0);
+}
+#endif
