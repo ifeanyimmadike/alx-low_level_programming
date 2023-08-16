4b5771c0230f02b929e058bcad35502be4867c40
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-15 20:48:08 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,13 @@
-#ifndef ma_H
-#define ma_H
 #include <stdio.h>
-#define 2-main.c 
 
 /**
- * mainf-function name
+ * main - prints the name of the file
  *
- * Return:0
+ * Return: Always 0 (Success)
  */
-int mainf(void)
+int main(void)
 {
-	printf("%d", 2-main.c);
+	printf("%s\n", __FILE__);
 	return (0);
 }
-#endif
+
