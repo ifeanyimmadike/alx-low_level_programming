8d149630aa78fa1c4ce78c1a7f9fa49371934a70
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-15 19:36:28 -0700

    define PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+#ifndef PI_H
+#define PI_H
+#include <stdio.h>
+#define PI 3.14159265359
+
+/**
+ * mainf-function name
+ *
+ * Return:0
+ */
+int mainf(void)
+{
+        printf("%d\n", PI);
+        return (0);
+}
+#endif
