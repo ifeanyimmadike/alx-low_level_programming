1c3a321c9481a145806ff57848850b70649e06a6
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-16 19:27:27 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,22 @@
  * print_name-function print name
  * @name:input name
  * @f:pointer
+ * Return:0
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
+	if(name == NULL && f == null ) 
 	f(name);
 }
-
+void (*ptr)(char *)
+{
+	ptr();
+}
+/**
+ * main-funct name
+ */
+void main(void)
+{
+	printf("%s", name)
+	return (0);
+}
