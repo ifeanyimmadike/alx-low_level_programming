38dcf4f642b77118d4604cb1918d883f2249b726
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-16 18:25:58 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_name-function print name
+ * @name:input name
+ * @f:pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s", name);
+}
+void (*p)(char *)
+{
+	p();
+}
+int main(void)
+{
+	p(name);
+}
