273b40edeee9ddb9695369276fb2e431dbeae031
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-22 18:36:48 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,7 +36,7 @@ void print_all(const char * const format, ...)
 					if (str == NULL)
 						printf("(nil)");
 					if (!str)
-						NULL;
+						str = NULL;
 					printf("%s", str);
 					break;
 				default:
