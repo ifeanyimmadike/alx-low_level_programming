a317e3185f3d8d8d68061b1a7a9140c5ed404562
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-27 21:08:33 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+STRUCT 
+TYDEF
+
+
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
-
-struct dog{
+/**
+ * struct dog-struct dog with name,age and owner
+ * @name: input 
+ * @age: input
+ * @owner: input
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner
