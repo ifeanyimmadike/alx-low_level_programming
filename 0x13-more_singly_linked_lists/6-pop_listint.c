54c297b19154b176427792ac3ec4a30100a50763
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-29 18:48:48 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node 
+ * pop_listint - deletes the head node
  * @head: pointer to the first element in the linked list
  *
  * Return: the data inside the elements that was deleted,
