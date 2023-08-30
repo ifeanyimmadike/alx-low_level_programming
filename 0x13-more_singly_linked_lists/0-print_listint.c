0374257cd34a5a56bda0e2c83a1b4db85b17c2bd
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-29 17:50:27 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_listint - prints available element
+ * @h: pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nm = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nm++;
+		h = h->next;
+	}
+
+	return (nm);
+}
+
