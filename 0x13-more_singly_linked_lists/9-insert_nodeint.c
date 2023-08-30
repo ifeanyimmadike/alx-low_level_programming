142ff9e9bdff401ace1c6af888dddbaa7dce1513
ifeanyimmadike <mmadikeifeanyi@gmail.com>
2023-08-29 19:20:41 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int j;
 	listint_t *new;
 	listint_t *temp = *head;
 
@@ -29,9 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (j = 0; temp && j < idx; i++)
 	{
-		if (i != NULL)
+		if (j == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
